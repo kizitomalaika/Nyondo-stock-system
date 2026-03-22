510a192e189005f300b033e3dfab9e685740d893
Malaika <kizitoangella8@gmail.com>
2026-03-22 17:11:32 +0300

    add headless flags for a pipeline

--- test_stock.py
+++ test_stock.py
@@ -12,6 +12,11 @@ BASE_URL = "https://kizitomalaika.github.io/Nyondo-stock-system/stock.html"
 def driver():
     """Set up chrome for each test"""
     chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(10)
     yield driver
